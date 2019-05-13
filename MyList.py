74ca995fd9733017ae695a02381697887d6b1c98
MdIqbalChowdhury <md.iqbal.chowdhury@verizon.com>
2019-05-13 17:30:27 -0400

    All my python class are here

---
+++ MyList.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+mylist= ["cat", "dog", 7]
+#print(f" I have a {mylist[0]} and a {mylist[1]} that are {mylist[2]}.")
+feline= mylist[0]
+#print(feline)
+print("my { } string ".format("first"))

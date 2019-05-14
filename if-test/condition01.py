899aa1a1c540e356283782c5298ba759da7770c7
MdIqbalChowdhury <md.iqbal.chowdhury@verizon.com>
2019-05-14 15:43:08 -0400

    Codes that testing if conditionals

---
+++ if-test/condition01.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+hostname= "MIG"
+if hostname == "MIG":
+    print(" The hostname was found to be mtg")
+
---
+++ if-test/condition2.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+hostname= input("what value should we set for hostame?")
+if hostname == "MTG":
+    print("The hostname was found to be mtg")
